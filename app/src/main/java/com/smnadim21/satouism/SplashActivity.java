3e4997403eb4918d43d12ca5d56274d5bc64994c
unknown <mizanursys94@gmail.com>
2020-07-26 03:21:43 +0600

    Robi subscription api

---
+++ app/src/main/java/com/smnadim21/satouism/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.smnadim21.satouism;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.smnadim21.satouism.advert.SP;
+
+import static com.smnadim21.satouism.advert.AdsLib.checkSubStatus;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkSubStatus(SP.getSubCode());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,ItemListActivity.class));
+                finish();
+            }
+        },2000);
+
+
+
+
+
+
+    }
+}
